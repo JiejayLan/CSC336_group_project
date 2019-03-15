ef15986ddfb03266d6e5f9d5202df85b5e4fa447
jieJayLan <lanjie3458@gmail.com>
2019-03-15 10:24:27 -0400

    add a grid box

--- controllers/jobs_controller.js
+++ controllers/jobs_controller.js
@@ -11,17 +11,53 @@ module.exports = (connection) => {
         ' SELECT *' +
         ' FROM Jobs' ;
     
-    connection.query(
-        
+    connection.query( 
         QUERY,
-        
         (error, results, fields) => {
             if (error) {
                 console.log(error)
                 res.json(error)
             } else {
+                let result = [  {
+                    job_ID: 1000,
+                    poster_ID: 103,
+                    job_title: 'front-end programmer',
+                    description: 'one of the best job',
+                    location: 'New York' },
+                 {
+                    job_ID: 1001,
+                    poster_ID: 104,
+                    job_title: 'MTA bus operator',
+                    description: 'A lot of money',
+                    location: 'Bronx' },
+                    {
+                        job_ID: 1000,
+                        poster_ID: 103,
+                        job_title: 'front-end programmer',
+                        description: 'one of the best job',
+                        location: 'New York' },
+                     {
+                    job_ID: 1001,
+                    poster_ID: 104,
+                    job_title: 'MTA bus operator',
+                    description: 'A lot of money',
+                    location: 'Bronx' },
+                    {
+                    job_ID: 1000,
+                    poster_ID: 103,
+                    job_title: 'front-end programmer',
+                    description: 'one of the best job',
+                    location: 'New York' },
+                    {
+                    job_ID: 1001,
+                    poster_ID: 104,
+                    job_title: 'MTA bus operator',
+                    description: 'A lot of money',
+                    location: 'Bronx' }                                    
+                ]
+                
               console.log('result is ',results);
-                res.render('pages/index')
+                res.render('pages/index',{results:result})
             }
         }
     
