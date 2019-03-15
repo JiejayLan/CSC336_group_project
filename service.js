344d21f07b60aecf2a5bda698228df1aad2c6bf9
Shi B <sh2581@nyu.edu>
2019-03-15 01:36:31 +0000

    Updated routing in service.js to match updated file structure

--- service.js
+++ service.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var mysql = require('mysql');
-var app = express();
-var outputString = "";
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 const port = process.env.PORT || 3003;
+
+
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
@@ -10,35 +12,19 @@ const query_test_create = 'CREATE TABLE Test(EmplId INT, Name varchar(100), Hire
 const query_test_select = 'SELECT * FROM Test ORDER BY EmplId;';
 const query_test_insert = 'INSERT INTO Test (EmplId, Name, HireDate) VALUES (343, "jie", "5-20");';
 
-var connection = mysql.createConnection({
+const CONNECTION = mysql.createConnection({
     host: 'jobfirstdatabase.c1vr39jujtbs.us-east-2.rds.amazonaws.com',
     user: "nooredin",
     password: 'nooredin',
     port: 3306,
     database: 'jobfirst_database'
-});
+})
 
-
-connection.connect(function(err)
-{
-    console.log("connecting");
-    if (err) {
-        console.log(err);
-        return;
-    }
-});
-
-
-app.get('/', function(req, res) {
-    connection.query(query_test_select, (error, results, fields) => {
-        console.log(results);
-        res.render('pages/index');
-        if (error) {
-            throw error;
-        }
-    });
-
-});
+//  Route handling.
+//  Each handler is in it's own file within ./controllers
+app.get('/', require('./controllers/index_controller.js')(CONNECTION));
+app.get('/users/:emplid', require('./controllers/get_user.js')(CONNECTION));
+app.get('/users/:id/jobs', require('./controllers/get_user_jobs_controller.js')(CONNECTION));
 
 // about page 
 app.get('/login', function(req, res) {
