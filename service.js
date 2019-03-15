a8a34d14470c37ea993d4b3d8c92931f61473a97
jieJayLan <lanjie3458@gmail.com>
2019-03-14 21:22:52 -0400

    Auto stash before merge of "master" and "origin/master"

--- service.js
+++ service.js
@@ -3,30 +3,30 @@ var mysql = require('mysql');
 var app = express();
 var outputString = "";
 const port = process.env.PORT || 3003;
-app.engine('html', require('ejs').renderFile);
+app.engine('html', require('handlebars').renderFile);
 app.set('view engine', 'ejs');
 
-const query_test_create = 'CREATE TABLE Test(EmplId INT, Name varchar(100), HireDate varchar(100));'
-const query_test_select = 'SELECT * FROM Test ORDER BY EmplId;';
-const query_test_insert = 'INSERT INTO Test (EmplId, Name, HireDate) VALUES (343, "jie", "5-20");';
+// const query_test_create = 'CREATE TABLE Test(EmplId INT, Name varchar(100), HireDate varchar(100));'
+// const query_test_select = 'SELECT * FROM Test ORDER BY EmplId;';
+// const query_test_insert = 'INSERT INTO Test (EmplId, Name, HireDate) VALUES (343, "jie", "5-20");';
 
-var connection = mysql.createConnection({
-    host: 'jobfirstdatabase.c1vr39jujtbs.us-east-2.rds.amazonaws.com',
-    user: "nooredin",
-    password: 'nooredin',
-    port: 3306,
-    database: 'jobfirst_database'
-});
+// var connection = mysql.createConnection({
+//     host: 'jobfirstdatabase.c1vr39jujtbs.us-east-2.rds.amazonaws.com',
+//     user: "nooredin",
+//     password: 'nooredin',
+//     port: 3306,
+//     database: 'jobfirst_database'
+// });
 
 
-connection.connect(function(err)
-{
-    console.log("connecting");
-    if (err) {
-        console.log(err);
-        return;
-    }
-});
+// connection.connect(function(err)
+// {
+//     console.log("connecting");
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+// });
 
 
 app.get('/', function(req, res) {
