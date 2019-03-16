e051f6a64bf5744349ee76dd63ae5ec1ccfb27a4
jieJayLan <lanjie3458@gmail.com>
2019-03-16 10:03:18 -0400

    modified logout timer

--- views/partials/logoutTimer.js
+++ views/partials/logoutTimer.js
@@ -1,6 +1,7 @@
+
 function idleTimer() {
-    let t;
-    console.log('it is time to logout');
+    let time;
+    console.log('time to logout')
     //window.onload = resetTimer;
     window.onmousemove = resetTimer; // catches mouse movements
     window.onmousedown = resetTimer; // catches mouse movements
@@ -12,15 +13,14 @@ function idleTimer() {
         window.location.href = '/logout';  //Adapt to actual logout script
     }
 
-    function reload() {
-            window.location = self.location.href;  //Reloads the current page
-    }
+   function reload() {
+        window.location = self.location.href;  //Reloads the current page
+   }
 
-    function resetTimer() {
-        clearTimeout(t);
-        t = setTimeout(logout, 5000);  // time is in milliseconds (1000 is 1 second)
-        t= setTimeout(reload, 5000);  // time is in milliseconds (1000 is 1 second)
+   function resetTimer() {
+        clearTimeout(time);
+        time = setTimeout(logout, 5000);  // time is in milliseconds (1000 is 1 second)
+        time= setTimeout(reload, 5000);  // time is in milliseconds (1000 is 1 second)
     }
 }
-console.log('it is time to logout');
 idleTimer();
