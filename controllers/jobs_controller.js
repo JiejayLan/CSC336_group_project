7b8ba59698d72d006a168dcb927be5c106f23748
jieJayLan <lanjie3458@gmail.com>
2019-03-16 22:22:09 -0400

    better query to search for jobs

--- controllers/jobs_controller.js
+++ controllers/jobs_controller.js
@@ -2,74 +2,46 @@
 
 module.exports = (connection) => {
   return (req, res) => {
-    let QUERY;
+    let query;
     let user_location =req.query.InputLocation; 
     let user_id = req.session.user.user_ID;
     let user_title =req.query.InputTitle;
-    let query ='';
-    let queryParam =[];
 
-    //how to use WHERE LIKE here to find the substring
-    if((user_location == null||user_location=='') ){
-        QUERY = 
-        ' SELECT job_ID, job_title, location FROM Jobs' ;
+
+    if((user_location === undefined)&&(user_title === undefined) ){
+        query =' SELECT job_ID, job_title, location FROM Jobs' ;
+        }
+    else if(user_location === undefined){
+        query= ' SELECT job_ID, job_title, location FROM Jobs WHERE' 
+        +' job_title LIKE' + '"%' + user_title + '%"'
+        ; 
+    }
+    else if(user_title === undefined){
+        query = 
+        ' SELECT job_ID, job_title, location FROM Jobs WHERE '
+        +'location ='
+        +'"'+user_location
+        + '"'
+        ' ;'; 
     }
     else{
-        QUERY = 
-        ' SELECT job_ID, job_title, location FROM Jobs WHERE location = ?;';  
-        queryParam =[user_location];       
+        console.log(4);
+        query= ' SELECT job_ID, job_title, location FROM Jobs WHERE' 
+        +' job_title LIKE' 
+        + '"%' + user_title 
+        + '%"'
+        +'AND '
+        +'location ='
+        +'"'+user_location+ '"'
+        ' ;';       
     }
-
     connection.query( 
-        QUERY,
-        queryParam,
+        query,
         (error, results, fields) => {
             if (error) {
                 console.log(error)
                 res.json(error)
             } else {
-                let result = [  {
-                    job_ID: 1000,
-                    poster_ID: 103,
-                    job_title: 'front-end programmer',
-                    description: 'one of the best job',
-                    location: 'New York' },
-                 {
-                    job_ID: 1001,
-                    poster_ID: 104,
-                    job_title: 'MTA bus operator',
-                    description: 'A lot of money',
-                    location: 'Bronx' },
-                    {
-                        job_ID: 1000,
-                        poster_ID: 103,
-                        job_title: 'front-end programmer',
-                        description: 'one of the best job',
-                        location: 'New York' },
-                     {
-                    job_ID: 1001,
-                    poster_ID: 104,
-                    job_title: 'MTA bus operator',
-                    description: 'A lot of money',
-                    location: 'Bronx' },
-                    {
-                    job_ID: 1000,
-                    poster_ID: 103,
-                    job_title: 'front-end programmer',
-                    description: 'one of the best job',
-                    location: 'New York' },
-                    {
-                    job_ID: 1001,
-                    poster_ID: 104,
-                    job_title: 'MTA bus operator',
-                    description: 'A lot of money',
-                    location: 'Bronx' }                                    
-                ]
-              if(user_title !='' && user_title !=null){
-                results = results.filter((job)=>{
-                    return job.job_title.includes(user_title);
-                  });  
-              }
               console.log('result is ',results);
               res.render('pages/index',{results:results, id:user_id})
             }
