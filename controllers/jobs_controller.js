228451bbf878d84ad6a27780a2c6acba03e5b76c
jieJayLan <lanjie3458@gmail.com>
2019-03-17 16:24:53 -0400

    1.add post job query
    2. now based on employer / employee and guest, the main page is different

--- controllers/jobs_controller.js
+++ controllers/jobs_controller.js
@@ -5,18 +5,22 @@ module.exports = (connection) => {
     let query;
     let user_location =req.query.InputLocation; 
     let user_id = req.session.user.user_ID;
+    let user_type = req.session.user.type_name;
     let user_title =req.query.InputTitle;
 
 
-    if((user_location === undefined)&&(user_title === undefined) ){
+    if((user_location === undefined||user_location === '')&&(user_title === undefined||user_title === '') ){
+        console.log('no keywork for search');
         query =' SELECT job_ID, job_title, location FROM Jobs' ;
         }
-    else if(user_location === undefined){
+    else if(user_location === undefined ||user_location === ''){
+        console.log('search for title');
         query= ' SELECT job_ID, job_title, location FROM Jobs WHERE' 
         +' job_title LIKE' + '"%' + user_title + '%"'
         ; 
     }
-    else if(user_title === undefined){
+    else if(user_title === undefined||user_title === ''){
+        console.log('search for location');
         query = 
         ' SELECT job_ID, job_title, location FROM Jobs WHERE '
         +'location ='
@@ -25,6 +29,7 @@ module.exports = (connection) => {
         ' ;'; 
     }
     else{
+        console.log('search for location adn title');
         query= ' SELECT job_ID, job_title, location FROM Jobs WHERE' 
         +' job_title LIKE' 
         + '"%' + user_title 
@@ -42,7 +47,7 @@ module.exports = (connection) => {
                 res.json(error)
             } else {
             //   console.log('result is ',results);
-              res.render('pages/index',{results:results, id:user_id})
+              res.render('pages/index',{results:results,user_type:user_type})
             }
         }
     
--- controllers/post_job_controller.js
+++ controllers/post_job_controller.js
@@ -1,27 +1,20 @@
 
 module.exports = (connection) => {
     const uuid = require('uuid/v1');
-    return (req, res) => {
-          
-        console.log(String.format("%040d", new BigInteger(UUID.randomUUID().toString().replace("-", ""), 16)));
+    return (req, res) => {      
         let query;
-        const JOB_ID = 656;
-        // const POSTER_ID = req.session.user.user_ID;
-        const POSTER_ID = 104;
+        const JOB_ID = Math.floor(Math.random() * 1000000000);
+        const POSTER_ID = req.session.user.user_ID;
         const JOB_DESCRIPTION = req.body.description;
         const JOB_LOCATION = req.body.location;
         const JOB_TITLE = req.body.title;
-        const PARAMATER = [JOB_ID,POSTER_ID ,JOB_TITLE ,JOB_DESCRIPTION,JOB_LOCATION];
         query = 
         'INSERT INTO Jobs(job_ID,poster_ID,job_title,description,location)'+
-        'VALUES ('+
-        JOB_ID +',' + '"'+POSTER_ID+'", "' + JOB_TITLE+ '","' + JOB_DESCRIPTION +'","' 
+        'VALUES ('+ JOB_ID +',' + '"'+POSTER_ID+'", "' + JOB_TITLE+ '","' + JOB_DESCRIPTION +'","' 
         + JOB_LOCATION+'");'
 
-        console.log(query);
         connection.query( 
             query,
-            // PARAMATER,
             (error, results, fields) => {
                 if (error) {
                     res.json(error)
@@ -31,37 +24,7 @@ module.exports = (connection) => {
             }
         
         );
-        // res.redirect('/');
-  
        
     }  
 }
 
-
-// INSERT INTO Jobs(job_ID,poster_ID,job_title,description,location)
-// VALUES
-//     (
-//         1000,
-//         103,
-//         'front-end programmer',
-//         'one of the best job',
-//         'New York'
-
-//     ),
-//     (
-//         1001,
-//         104,
-//         'MTA bus operator',
-//         'A lot of money',
-//         'Bronx'
-//     );
-
-
-// query= ' SELECT job_ID, job_title, location FROM Jobs WHERE' 
-// +' job_title LIKE' 
-// + '"%' + user_title 
-// + '%"'
-// +'AND '
-// +'location ='
-// +'"'+user_location+ '"'
-// ' ;';   
