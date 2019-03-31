6b786797f1842391db849005be0b6f158e5730ed
hongzhi pan <victoryhongzhi@gmail.com>
2019-03-31 18:42:49 -0400

    deleted unnessary controller files

--- controllers/sign_up_employee_controller.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = (connection) => {
-
-    return (req, res) => {
-        console.log(req.body);
-        console.log(req.params);
-        res.render('pages/sign_up_employee');
-    };
-}
