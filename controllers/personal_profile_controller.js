b13e86b2cde9ed9dcb620ff28a8fbb6f0614fd2a
albert6051 <albert.chen6051@gmail.com>
2019-04-09 08:42:19 -0400

    update applied job and language speak

--- controllers/personal_profile_controller.js
+++ controllers/personal_profile_controller.js
@@ -5,39 +5,65 @@ module.exports = (connection) => {
     let page_id = req.params.id;
     let user_id = req.session.user.user_ID;
     let query = "SELECT * "
-    + "FROM User NATURAL JOIN Employee "
-    + "WHERE employee_ID = "+page_id+" AND user_ID = "+page_id+";";
+      + "FROM User NATURAL JOIN Employee "
+      + "WHERE employee_ID = " + page_id + " AND user_ID = " + page_id + ";";
 
     let query1 = "SELECT * "
-    + "FROM Follow JOIN User ON followed_ID = user_ID "
-    + "WHERE follower_ID = "+page_id+";"
+      + "FROM Follow JOIN User ON followed_ID = user_ID "
+      + "WHERE follower_ID = " + page_id + ";"
 
-  
+    let query2 = "SELECT * "
+      + "FROM Speak NATURAL JOIN Language "
+      + "WHERE person_ID = " + page_id + ";"
 
-    connection.query( 
+    let query3 = "SELECT * "
+    + "FROM Applied JOIN Jobs ON applied_jobID = job_ID "
+    + "WHERE applicant_ID = " + page_id + ";"
+
+    connection.query(
       query,
       (error, results, fields) => {
-          if (error) {
-              console.log(error)
-              res.json(error)
-          } else {
-            //console.log('result is ',results);
-            let user = results;
-            connection.query( 
-              query1,
-              (error, results, fields) => {
-                  if (error) {
+        if (error) {
+          console.log(error)
+          res.json(error)
+        } else {
+          //console.log('result is ',results);
+          let user = results;
+          connection.query(
+            query1,
+            (error, results, fields) => {
+              if (error) {
+                console.log(error)
+                res.json(error)
+              } else {
+                //console.log('follow is ',results);
+                let follow = results;
+                connection.query(
+                  query2,
+                  (error, results, fields) => {
+                    if (error) {
                       console.log(error)
                       res.json(error)
-                  } else {
-                      //console.log('follow is ',results);
-                      let follow = results;
-                      console.log('user_id is ',user_id);
-                      console.log('page_id is ',page_id);
-                      res.render('pages/personal_profile', {user:user[0], id:page_id, follow:follow, user_id:user_id});
+                    } else {
+                      let language = results;
+                      connection.query(
+                        query3,
+                        (error, results, fields) => {
+                          if (error) {
+                            console.log(error)
+                            res.json(error)
+                          } else {
+                            let applied = results;
+                            res.render('pages/personal_profile', { user: user[0], id: page_id, follow: follow, user_id: user_id, language: language, applied:applied });
+                          }
+                        }
+                      )
+                    }
                   }
-              });
-            }
-          });
-    }
+                )
+              }
+            });
+        }
+      });
+  }
 }
