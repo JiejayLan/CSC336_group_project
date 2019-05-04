093159fe8fff8e82a63974a2087859b13b3049d4
hongzhi pan <victoryhongzhi@gmail.com>
2019-05-04 19:58:49 -0400

    replaced queries with procedures

--- controllers/job_des_controller.js
+++ controllers/job_des_controller.js
@@ -5,17 +5,20 @@ module.exports = (connection) => {
         let poster_ID;
         let followed_list ;
         let status = 'unfollowed';
+        let job_id = req.params.job_id;
+        let user_ID = req.session.user.user_ID;
         //let query;
         // let query = 'SELECT * FROM Jobs WHERE job_ID='
         //             + req.params.job_id;
         // let query = 'SELECT * FROM Jobs NATURAL JOIN Employer WHERE job_ID='
         //             + req.params.job_id;
-        let query = 'SELECT * FROM Jobs JOIN Employer ON poster_ID = employer_ID WHERE job_ID='
-                    + req.params.job_id;
-        // let query2 = 'SELECT followed_ID FROM Follow WHERE follower_ID='
-        //              + poster_ID;
+        // let query = 'SELECT * FROM Jobs JOIN Employer ON poster_ID = employer_ID WHERE job_ID='
+        //             + req.params.job_id;
+        let query1 = 'CALL spGetJobDescription1(?);'
+        // let query2 = 'CALL PROCEDURE spGetJobDescription2(?);'
         connection.query(
-            query,
+            query1,
+            [job_id],
             (error, results, fields) => {
                 if(error){
                     console.log(error)
@@ -27,12 +30,14 @@ module.exports = (connection) => {
                     // const poster_id = results.poster_ID;
                     // const location = results.location;
                     poster_ID = results[0].poster_ID
-                    let query2 = 'SELECT followed_ID FROM Follow WHERE follower_ID='
-                     + req.session.user.user_ID;
+                    // let query2 = 'SELECT followed_ID FROM Follow WHERE follower_ID='
+                    //  + req.session.user.user_ID;
+                    let query2 = 'CALL spGetJobDescription2(?);'
                      //console.log('poster_ID is: ', poster_ID)
                      if(req.session.user.user_ID != undefined){
                         connection.query(
                             query2,
+                            [user_ID],
                             (error, result, fields) => {
                                 if(error){
                                     console.log(error)
