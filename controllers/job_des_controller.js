416b9f85d2e0e1809380484732e3e1006146e720
hongzhi pan <victoryhongzhi@gmail.com>
2019-05-04 22:10:21 -0400

    replace with procedures and fixed query

--- controllers/job_des_controller.js
+++ controllers/job_des_controller.js
@@ -12,19 +12,21 @@ module.exports = (connection) => {
         //             + req.params.job_id;
         // let query = 'SELECT * FROM Jobs NATURAL JOIN Employer WHERE job_ID='
         //             + req.params.job_id;
-        // let query = 'SELECT * FROM Jobs JOIN Employer ON poster_ID = employer_ID WHERE job_ID='
-        //             + req.params.job_id;
-        let query1 = 'CALL spGetJobDescription1(?);'
+        let query = 'SELECT * FROM Jobs JOIN Employer ON poster_ID = employer_ID WHERE job_ID='
+                    + req.params.job_id;
+        // let query = 'CALL spGetJobDescription1(?);'
         // let query2 = 'CALL PROCEDURE spGetJobDescription2(?);'
         connection.query(
-            query1,
-            [job_id],
+            // query,
+            // [req.params.job_id],
+             query,
             (error, results, fields) => {
                 if(error){
                     console.log(error)
                     res.json(error)
+                    console.log('job description1 error')
                 } else{
-                    console.log("result is ", results)
+                    console.log("job description result is ", results)
                     // const description = results.description;
                     // const job_title = results.job_title;
                     // const poster_id = results.poster_ID;
@@ -34,17 +36,19 @@ module.exports = (connection) => {
                     //  + req.session.user.user_ID;
                     let query2 = 'CALL spGetJobDescription2(?);'
                      //console.log('poster_ID is: ', poster_ID)
-                     if(req.session.user.user_ID != undefined){
+                    if(req.session.user.user_ID != undefined){
                         connection.query(
                             query2,
-                            [user_ID],
+                            [req.session.user.user_ID],
+                            // query2,
                             (error, result, fields) => {
                                 if(error){
                                     console.log(error)
+                                    console.log('job description2 error')
                                 } else {
                                     followed_list = result
                                     //console.log("this is posterid:", poster_ID)
-                                    console.log("this is ", followed_list)
+                                    console.log("this is job desciption ", followed_list)
                                     followed_list.forEach(function(followed){
                                         if(followed.followed_ID == poster_ID){
                                             status = 'followed'
